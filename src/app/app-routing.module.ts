0e27f5a050f78df8f3f0833e7f2d7c65ba0be449
Selvam Subramani <muthuselvam1987@gmail.com>
2019-07-16 07:43:43 -0700

    Routing & common components are done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AddComponent } from './ui/add/add.component';
+import { ViewComponent } from './ui/view/view.component';
+import { EditComponent } from './ui/edit/edit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'add', component: AddComponent },
+  { path: 'edit', component: EditComponent },
+  { path: 'view', component: ViewComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const RoutingComponents = [AddComponent, EditComponent, ViewComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, RoutingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AddComponent } from './ui/add/add.component';
-import { ViewComponent } from './ui/view/view.component';
-import { EditComponent } from './ui/edit/edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddComponent,
-    ViewComponent,
-    EditComponent
+    RoutingComponents
   ],
   imports: [
     BrowserModule,
